3b150b3f8ce044857ecf283ce8e23f8cdfd503c6
Elmer Jacobo <88252912+elmerjacobo97@users.noreply.github.com>
2026-08-08 10:47:21 -0500

    Update BrandMark.tsx

--- components/layout/BrandMark.tsx
+++ components/layout/BrandMark.tsx
@@ -13,13 +13,13 @@ export function BrandMark() {
         <path
           d="M4 4V28M4 4H18M4 16H14M4 28H14"
           className="stroke-text transition-colors duration-300 group-hover:stroke-accent group-focus-visible:stroke-accent"
-          strokeWidth="3"
+          strokeWidth="5"
           strokeLinecap="square"
         />
         <path
           d="M18 4H28V21C28 25.5 24.5 28 19.5 28"
           className="stroke-accent transition-colors duration-300 group-hover:stroke-text group-focus-visible:stroke-text"
-          strokeWidth="3"
+          strokeWidth="5"
           strokeLinecap="square"
           strokeLinejoin="miter"
         />
